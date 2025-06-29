322a917bb6f19b7ef8cabb9fc74a966a3a026cad
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-06-29 01:22:28 +0100

    commit

--- app/components/auth.jsx
+++ app/components/auth.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react'
+import { supabase } from '../lib/supabaseClient';
 
 const Auth = () => {
+    const [isSignUp, setIsSignUp] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (isSignUp) {
+            const { error: signingUpError } = await supabase.auth.signUp({email, password});
+            if (signingUpError) {
+                console.error("Error signing up", signingUpError.message);
+                return;
+            }
+        } else {
+            const {error: signingInError } = await supabase.auth.signInWithPassword({email, password});
+            if (signingInError) {
+                console.error("Error signing In", signingInError.message);
+                return;
+            }
+        }
+
+        setEmail("")
+        setPassword("")
+    }
 
   return (
-    <div className=' flex flex-col items-center space-y-5'>
-        <h2 className=' font-bold text-2xl'>
-            Sign In
+    <div className=' flex flex-col items-center pb-8'>
+        <h2 className=' font-bold text-2xl mb-5'>
+            {isSignUp ? "Sign Up" : "Sign In"}
         </h2>
 
-        <form className=' flex flex-col w-full items-center space-y-2'>
+        <form onSubmit={handleSubmit} className=' flex flex-col w-full items-center space-y-2 mb-3 text-sm'>
             <input 
                 type="email"
                 placeholder='Email'
-                className=' border-1 border-gray-500 rounded-sm py-2 px-4 w-3/4'
+                onChange={(e) => setEmail(e.target.value)}
+                className=' border-1 border-gray-500 rounded-sm py-1.5 px-4 w-3/4'
             />
             <input 
                 type="password"
                 placeholder='Password'
-                className=' border-1 border-gray-500 rounded-sm py-2 px-4 w-3/4'
+                onChange={(e) => setPassword(e.target.value)}
+                className=' border-1 border-gray-500 rounded-sm py-1.5 px-4 w-3/4'
             />
-            <button className=' py-2 px-4 bg-blue-500 rounded-sm hover:bg-blue-600 transition-normal'>
-                Sign In
+            <button type='submit' className=' py-2 px-4 bg-blue-500 rounded-sm hover:bg-blue-600 transition-normal'>
+                {isSignUp ? "Sign Up" : "Sign In"}
             </button>
         </form>
+        <button onClick={() => setIsSignUp(!isSignUp)} className=' text-gray-300 text-sm hover:border-1 hover:border-blue-400 
+        hover:py-1.5 hover:px-4 hover:rounded-sm'>
+            {isSignUp ? "Already have an accout, Sign In" : "Don,t have an account, Sign Up"}
+        </button>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,49 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import Auth from "./components/auth";
 import TaskManager from "./components/taskManager";
+import { supabase } from "./lib/supabaseClient";
 
 
 export default function Home() {
-  
+  const [session, setSession] = useState(null);
+
+  const fetchSession = async () => {
+    const currentSession = await supabase.auth.getSession();
+    console.log(currentSession)
+    setSession(currentSession.data.session);
+  }
+
+  useEffect(() => {
+    fetchSession();
+
+    const {data: authListener} = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
+    );
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+
+  }, []);
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  }
+
   return (
     <main className=" max-w-2xl m-auto">
-      <TaskManager />
-      <Auth />
+      {
+        session ? 
+          (<>
+            <button onClick={handleLogout} className=" hover:bg-gray-800 border-1 rounded-sm py-1 px-4 mt-8">Log Out</button>
+            <TaskManager /> 
+          </>) :
+          (<Auth />)
+      }
     </main>
   );
 }
